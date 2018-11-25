087ecd0c9fedd6a5d52daee5e866ac2281841134
Alpha1071 <44518454+Alpha1071@users.noreply.github.com>
2018-11-25 07:14:57 +0300

    add py file

---
+++ TEXT GAME.py
@@ -0,0 +1,17 @@
+import pygame
+import time
+
+pygame.init()
+pygame.mixer.music.load("anticipation.mp3")
+pygame.mixer.music.play()
+time.sleep(2)
+
+print(r" _______   ______  __   __  _______   _____   _    _   __  __ ")
+print(r"|__   __| |  ____| \ \ / / |__   __| |_   _| | |  | | |  \/  |")
+print(r"   | |    | |__     \ V /     | |      | |   | |  | | | \  / |")
+print(r"   | |    |  __|     > <      | |      | |   | |  | | | |\/| |")
+print(r"   | |    | |____   / . \     | |     _| |_  | |__| | | |  | |")
+print(r"   |_|    |______| /_/ \_\    |_|    |_____|  \____/  |_|  |_|")
+
+time.sleep(1)
+print("Merhaba oyuncu bu oyunda doğru seçimleri yapıp puan toplaman lazım.")
